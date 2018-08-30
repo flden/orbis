45b56bbb0ff8a98555f75cd4d29bf2c0ad2f6830
den <graycalx2@gmail.com>
2018-08-30 18:03:03 +0000

    123

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,15 @@ import Vue from 'vue';
 
 const leftSlider = new Swiper('.left-slider__container', {
   direction: 'vertical',
-  //simulateTouch: false
+  simulateTouch: false,
+  autoplay: true,
+  effect: 'fade'
 });
 
 const rightSlider = new Swiper('.right-slider__container', {
   direction: 'vertical',
   slidesPerView: 4,
-  //simulateTouch: false,
+  simulateTouch: false,
   slideToClickedSlide: true,
 });
 
@@ -18,6 +20,16 @@ const clientSlider = new Swiper('.about__clients-list', {
   loop: true,
   spaceBetween: 16,
 });
+
+const topSlidesList = Array.from(document.querySelectorAll('.right-slider__slide'));
+
+topSlidesList.map(slide => {
+  slide.addEventListener('click', event => {
+    console.log(event.target)
+    event.target.closest('.right-slider__slide').querySelector('.right-slider__slide-desc-wrap').classList.toggle('hide');
+  })
+})
+
 
 leftSlider.controller.control = rightSlider;
 rightSlider.controller.control = leftSlider;
